10b0a67713d0e03616e977f89c4f2e944d134989
diankong <diankong@users.noreply.github.com>
2015-07-06 20:42:27 +0800

    Update ngTagEditor.js
    removed output attr from opions, add readonly attr

--- ngTagEditor.js
+++ ngTagEditor.js
@@ -31,24 +31,38 @@ angular.module('ngTagEditor', [])
 				tags: '=ngModel'
 			},
 			replace: true,
-			templateUrl: 'ngTagEditor.html',
+			templateUrl: 'template/ngTagEditor.html',
 			controller: ['$scope', '$attrs', '$element', '$http', '$filter', function($scope, $attrs, $element, $http, $filter){
 				
-				$scope.options = [];
-				$scope.options.output = $attrs.output || 'name';
-				$scope.options.fetch = $attrs.fetch || 'suggestions.php?q=';
-				$scope.options.placeholder = $attrs.placeholder || 'Enter a few letters...';
+				$scope.options = {};
+				console.log("$attrs.readonly is: " + $attrs.readonly);
+				if ($attrs.readonly === "true")
+				{
+				  $scope.options.readonly = true;
+				}
+				else
+				  $scope.options.readonly = false;
+				
+				$scope.options.fetch = $attrs.fetch || '';
+				$scope.options.placeholder = $attrs.placeholder || '';
 				$scope.options.apiOnly = $attrs.apiOnly || false;
 				$scope.search = '';
+				
+				console.log("typeof readonly : " + typeof $scope.options.readonly);
+				$scope.tagClass = $scope.options.readonly?"readonly":"tag";
+				console.log("tagClass is: " + $scope.tagClass);
 			
 				$scope.$watch('search', function(){
-					$http.get($scope.options.fetch + $scope.search).success(function(data){
-						$scope.suggestions = data.data;
-						/* console.log(data); */
-					});
+				  if ($scope.options.fetch && $scope.options.fetch.length>0){
+  					$http.get($scope.options.fetch + $scope.search).success(function(data){
+  						$scope.suggestions = data.data;
+  						/* console.log(data); */
+  					});
+				  }
+
 				});
-				$scope.add = function(id, name){
-					$scope.tags.push({'id':id, 'name':name});
+				$scope.add = function(tag){
+					$scope.tags.push(tag);
 					$scope.search = '';
 					$scope.$apply();
 				};
@@ -56,26 +70,34 @@ angular.module('ngTagEditor', [])
 					$scope.tags.splice(index, 1);
 				};
 				
-				$element.find('input').on('keydown', function(e){
-					var keys = [8, 13, 32];
-					if(keys.indexOf(e.which) !== -1){
-						if(e.which == 8){ /* backspace */
-							if($scope.search.length === 0 && $scope.tags.length){
-								$scope.tags.pop();
-								e.preventDefault();
-							}
-						}
-						else if(e.which == 32 || e.which == 13){ /* space & enter */
-							if($scope.search.length && !$scope.apiOnly){
-								if(!$scope.apiOnly){
-									$scope.add(0, $scope.search);
-									e.preventDefault();
-								}
-							}
-						}
-						$scope.$apply();
-					}
-				});
+				console.log("$scope.options.readonly: " + $scope.options.readonly);
+				if (!$scope.options.readonly)
+				{
+				  console.log("监听");
+  				$element.find('input').on('keydown', function(e){
+  					var keys = [8, 13, 32];
+  					if(keys.indexOf(e.which) !== -1){
+  						if(e.which == 8){ /* backspace */
+  							if($scope.search.length === 0 && $scope.tags.length){
+  								$scope.tags.pop();
+  								e.preventDefault();
+  							}
+  						}
+  						else if(e.which == 32 || e.which == 13){ /* space & enter */
+  							if($scope.search.length && !$scope.apiOnly){
+  								if(!$scope.apiOnly){
+  									$scope.add($scope.search);
+  									e.preventDefault();
+  								}
+  							}
+  						}
+  						$scope.$apply();
+  					}
+  				});
+				}
+
+
+
 				
 			}]
 		}
